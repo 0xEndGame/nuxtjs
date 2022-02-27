d9f423db0361f3b3255fc0d22bee4160ae58337c
Merkel <you@example.com>
2022-02-27 19:18:14 +0100

    5

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome
-    <nuxt-img src="/davinci.gif" /> />
+    <figure>
+    <img src="davinci.gif" style="visibility:visible;height:100%;left:0;position:absolute;top:0;width:100%" >
+    </figure>
   </div>
 </template>
